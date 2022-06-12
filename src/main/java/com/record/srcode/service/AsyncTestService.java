332efdc201d557e015c2a3042da84a62b86c54a1
lifezq <lifezqy@126.com>
2022-06-12 21:19:29 +0800

    async execute

--- src/main/java/com/record/srcode/service/AsyncTestService.java
+++ src/main/java/com/record/srcode/service/AsyncTestService.java
@@ -8,11 +8,11 @@ public class AsyncTestService {
 
     @Async
     public void executeAsync1(Integer i) {
-        System.out.println("异步输出1:" + i);
+        System.out.println(Thread.currentThread().getName() + "异步输出1:" + i);
     }
 
     @Async
     public void executeAsync2(Integer i) {
-        System.out.println("异步输出2:" + i);
+        System.out.println(Thread.currentThread().getName() + "异步输出2:" + i);
     }
 }
